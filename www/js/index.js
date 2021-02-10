7bcd3ddf438c0d6ee0528953dc269dac48cb5e1e
Biswajit Biswas <jfbiswajit@gmail.com>
2021-02-10 14:07:40 +0600

    Add: works as expected

--- www/js/index.js
+++ www/js/index.js
@@ -1,58 +1,32 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    document.getElementById('print').addEventListener('click', function() {
-      printNow();
-    });
+  document.getElementById('print').addEventListener('click', function () {
+    printNow();
+  });
 }
-
 
 function printNow() {
   BTPrinter.list(
     function (data) {
-      alert(JSON.stringify(data));
+      BTPrinter.connect(
+        function (data) {
+          BTPrinter.printText(
+            function (data) {
+              BTPrinter.disconnect(
+                function (data) {},
+                function (err) {},
+                'PrinterName'
+              );
+            },
+            function (err) {},
+            'String to Print'
+          );
+        },
+        function (err) {},
+        'RPP300'
+      );
     },
-    // function (err) {
-    //   alert('Error');
-    //   alert(err);
-    // }
-
-    BTPrinter.connect(
-      function (data) {
-        alert('Success');
-        alert(data);
-        
-        BTPrinter.printText(
-          function (data) {
-            console.log('Success');
-            console.log(data);
-          },
-          function (err) {
-            console.log('Error');
-            console.log(err);
-          },
-          'This Text Comes From The Fucking Printer!'
-        );
-        
-        BTPrinter.disconnect(
-          function (data) {
-            console.log('Success');
-            console.log(data);
-          },
-          function (err) {
-            console.log('Error');
-            console.log(err);
-          },
-          'RPP300'
-        );
-      },
-      function (err) {
-        alert('Error');
-        alert(err);
-      },
-      'RPP300'
-    )
-    
-    
+    function (err) {}
   );
 }
