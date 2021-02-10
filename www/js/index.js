ccd36b72e4fa9c0d6f7173385ceff67fc2188b2e
Biswajit Biswas <jfbiswajit@gmail.com>
2021-02-10 14:22:46 +0600

    Add: works as expected

--- www/js/index.js
+++ www/js/index.js
@@ -12,15 +12,18 @@ function printNow() {
       BTPrinter.connect(
         function () {
           BTPrinter.printText(
-            function () {
+            function (data) {
+              
               BTPrinter.disconnect(
                 function () {},
                 function (err) {},
                 'RPP300'
               );
             },
-            function (err) {},
-            `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAVCAAAAACMfPpKAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAfElEQVQYlU2QWwrEMAwDR0vuf+XZj8qJSyjIyNYjAkAMQNFhkBCKzoNiin70kxKBN41ENuf7+9AZWQOGRx/2m4TeKy2YO0GyDpwszW5EUCs/ur78NZtGvSa8azdPDGttsonot8LtDFNnrs4yLSbuJk0ajnV3vevhCxUj4Q+R11n764g4WgAAAABJRU5ErkJggg==`
+            function (err) {
+              
+            },
+            'Image Base64 String'
           );
         },
         function (err) {},
