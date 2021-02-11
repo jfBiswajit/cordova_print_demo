ef23fd142c459aaa984e585a4c47d4cae756b8e3
Biswajit Biswas <jfbiswajit@gmail.com>
2021-02-11 18:00:07 +0600

    Add

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,25 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-plugin-bluetooth-printer.BluetoothPrinter",
-      "file": "plugins/cordova-plugin-bluetooth-printer/www/BluetoothPrinter.js",
-      "pluginId": "cordova-plugin-bluetooth-printer",
-      "clobbers": [
-        "BTPrinter"
-      ]
-    },
-    {
       "id": "cordova-plugin-btprinter.BluetoothPrinter",
       "file": "plugins/cordova-plugin-btprinter/www/BluetoothPrinter.js",
       "pluginId": "cordova-plugin-btprinter",
       "clobbers": [
         "BTPrinter"
       ]
+    },
+    {
+      "id": "cordova-pdf-generator.pdf",
+      "file": "plugins/cordova-pdf-generator/www/pdf.js",
+      "pluginId": "cordova-pdf-generator",
+      "clobbers": [
+        "cordova.plugins.pdf",
+        "pugin.pdf",
+        "pdf"
+      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-bluetooth-printer": "0.0.1-dev",
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-btprinter": "0.1.0-dev"
+    "cordova-plugin-btprinter": "0.1.0-dev",
+    "cordova-pdf-generator": "2.1.1"
   };
 });
